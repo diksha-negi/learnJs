a03771799289dd3e55d8699ac2d5414bb4841e2d
Diksha-negi <123229517+diksha-negi@users.noreply.github.com>
2025-01-12 14:19:46 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+// let score = "33abc" => NaN
+// let score = "33" => 33
+// let score = null
+// let score = undefined
+// let score = true => 1
+// let score = "hitesh"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn = 1 => true
+// let isLoggedIn = "" => false
+// let isLoggedIn = "hitesh" => true
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
