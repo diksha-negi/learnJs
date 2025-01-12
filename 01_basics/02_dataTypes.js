94315de493d8208b4ef2007950361a3bcdcb370f
Diksha-negi <123229517+diksha-negi@users.noreply.github.com>
2025-01-12 13:51:53 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+//alert("hello") //we are using nodejs, not browser
+
+console.log(3
+    +
+    3
+    ) //code readability should be high
+console.log("diksha")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+//numder => 2 to power 53
+//bigint
+//string =>" "
+//boolean => true/false
+//null =>standalone value
+// undefined => 
+//symbol => unique
+//object
+
+console.log(typeof null)   //object
+console.log(typeof undefined);   //undefined
