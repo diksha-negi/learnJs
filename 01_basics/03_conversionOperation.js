304402c91669e4a1a6c1d880d7f7c5af17f77640
Diksha-negi <123229517+diksha-negi@users.noreply.github.com>
2025-01-12 15:42:39 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,3 +23,48 @@
 // let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+/**************************************** Operations ********************************************** */
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " hitesh"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// let a= 2+1+"2"
+// console.log(typeof(a))
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true); =>1
+// console.log(true+); =>error
+// console.log(+""); =>0
+
+// let num1 = num2 = num3
+// num1 = num2 = num3 = 2+2
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+let x = 3;
+let y = x++;
+console.log(x,y);
+
+let a = 3;
+let b = ++a;
+console.log(a,b);
