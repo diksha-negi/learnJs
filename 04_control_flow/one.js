8b1b4cffc5d24ca574956472a1ef5c296a266d06
Diksha-negi <123229517+diksha-negi@users.noreply.github.com>
2025-01-18 17:20:29 +0000

    controlflow statement

---
+++ 04_control_flow/one.js
@@ -0,0 +1,40 @@
+//if
+const isUserloggedIn = true
+const temperature = 41
+// if(2=="2"){
+// console.log("execute");
+// }
+
+//2<=2
+//<, >, <=, >=, ==, !=, ===, !==
+
+const score = 200
+// if(score>100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+const balance = 1000
+//if(balance > 500) console.log("test") , console.log("test2");
+
+// if(balance < 500) {
+//     console.log("less than 500");
+// }else if(balance <750){
+//     console.log("less than 750");
+// }else{
+//     console.log("hulululu")
+// }
+
+
+const userLoggedIn = true
+const debitCard = true 
+const loggedInFromGoogle = false
+const loggedInFromEmail = true 
+if(userLoggedIn && debitCard && 2==3){
+    console.log("Allow to buy course")
+}
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("user logged in")
+}
