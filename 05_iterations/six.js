2c6e2df215ccd5aee78fe4bf923808def6a3209a
Diksha-negi <123229517+diksha-negi@users.noreply.github.com>
2025-01-19 14:45:12 +0000

    foreach, filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,39 @@
+//const coding = ["js", "ruby", "java", "python", "cpp"]
+
+
+// const values= coding.forEach((item)=>{
+//     console.log(item)
+//     return item
+// })
+// console.log(values);
+
+
+
+//filter
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.filter( (num) => num > 4)
+// const newNums = myNums.filter( (num) =>
+//      {
+//         return num > 4
+//     }
+// )
+
+// const newNums = []
+
+// myNums.forEach( (num) => {
+//     if(num > 4){
+//         newNums.push(num)
+//     }
+// })
+
+// console.log(newNums);
+
+/*
+let userBooks = book.filter( (bk)=>bk.genre === 'History')
+console.log(userBooks);
+
+userBooks = books.filter( (bk)=> bk.publish >=2000)
+
+
+
